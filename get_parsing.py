eec7d853944df4fc54b4716689a94a8054a20b84
ivan <ivann@mail.ru>
2026-05-26 17:04:41 +0700

    Т.к. поменялось API подкорректировано под него. Парсим специалисто и школьников с сайта, готовый функционал. Только забыл убрать сохранение и возрат пандаса

--- get_parsing.py
+++ get_parsing.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import requests
 import pandas as pd
+import login
 
 
 def parsing(parse_type="All"):
@@ -13,28 +14,42 @@ def parsing(parse_type="All"):
 
     # Текущая дата и время, текущая
     now = datetime.now()
-    formatted_date = now.strftime("%Y-%m-%dT%H:%M:%S")
+    formatted_date = now.strftime("%Y-%m-%d")
 
+    # Получение токена
+    # login.start()
     # Токен
     with open('token.txt', 'r', encoding="utf-8") as file:
         token = file.read()
 
-    url = "https://api.evcg.ru/api/psych-tests/completed-tests"
+    # Запрос
+    # Выбор URL в зависимости от типа
+    if parse_type == "Specialist":
+        url = "https://api.evcg.ru/api/specialists/completed-tests"
+    elif parse_type == "Pupil":
+        url = "https://api.evcg.ru/api/pupils/completed-tests"
+    else:
+        # Для "All" пока заглушка!!!!!
+        print("Режим 'All' требует отдельной обработки")
+        return None
+  
     params = {
         "type": parse_type,
-        "startDate": "2026-04-01T00:00:00",
+        "startDate": "2026-04-01",
         "endDate": formatted_date
     }
     headers = {
         "Authorization": f"Bearer {token}"
     }
 
+    # Если не достучался, то None
     try:
         response = requests.get(url, params=params, headers=headers, timeout=10)
         data = response.json()
         # print(f"Найдено пользователей: {len(data)}")
     except requests.exceptions.ReadTimeout:
         return None
+    
     # список нужных тестовых значений
     necessary = ["extravag_introver_score",
                  "neirotizm_score", "company_worker",
@@ -42,26 +57,57 @@ def parsing(parse_type="All"):
                  "resource_investigator", "monitor_evaluation",
                  "team_worker", "completer_finisher",
                  "engineering_thinking_level"]
+    
     # user_list - список где будет храниться значения
     user_list = []
+
     for user in data:
         # user_dic - словарь каждого человека
-        user_dic = dict()
-        # Id и имя (его может не быть...)
+        user_dic = {}
+
+        # Id 
         user_dic["Id"] = user.get('accountId')
-        user_dic["fullName"] = user.get('fullName', None)
+
+        # Извлекаем данные в зависимости от 
+        # специалист это
+        if parse_type == "Specialist" and 'specialist' in user:
+            specialist = user['specialist']
+            parts = [specialist.get('surname', ''),
+                    specialist.get('name', ''),
+                    specialist.get('patronymic', '')]
+            # ФИО, если есть
+            user_dic["fullName"] = " ".join(filter(None, parts))
+            user_dic["profession"] = specialist.get('profession')
+        # или школьник
+        elif parse_type == "Pupil" and 'pupil' in user:
+            pupil = user['pupil']
+            parts = [pupil.get('surname', ''),
+                    pupil.get('name', ''),
+                    pupil.get('patronymic', '')]
+            # ФИО, если есть
+            user_dic["fullName"] = " ".join(filter(None, parts))
+            # день рождения
+            user_dic["birthday"] = pupil.get('birthday')
+            # гендер
+            user_dic["gender"] = pupil.get('gender')
+
         # Вытаскиваем параметры
         for test in user.get('psychTests', []):        
             for param in test.get('psychParams', []):
+                # берем только те, что нам нужны для кластер.
                 if param.get('name') in necessary:
                     user_dic[param.get('name')] = param.get('param')
         user_list.append(user_dic)
+
+    # в датаферйм
     df_user = pd.DataFrame(user_list)
+    # Удаляем строки, где есть хотя бы одно пропущенное значение
+    # возможно это нужно перенести в другой файл
+    df_user = df_user.dropna()
+    # печать и сохранение, временно
     df_user.to_csv("test.csv", index=False, encoding="utf-8")
-    print(df_user)
-             
+    print(df_user)       
 
-
-    
-
-parsing("ALL")
+# parsing("Specialist")
+# Pupil - ученики
+# Specialist - специалисты 
--- login.py
+++ login.py
@@ -1,11 +1,13 @@
 import requests
 
-url = "https://api.evcg.ru/api/auth/login"
-data = {
-    "email": "lubuntum@gmail.com",  
-    "password": "123456789"         
-}
+def start():
 
-response = requests.post(url, json=data)
-print("Статус:", response.status_code)
-print("Токен:", response.text)
+    url = "https://api.evcg.ru/api/auth/login"
+    data = {
+        "email": "lubuntum@gmail.com",  
+        "password": "123456789"         
+    }
+
+    response = requests.post(url, json=data)
+    with open('token.txt', 'w', encoding="utf-8") as file:
+        file.write(response.text)
