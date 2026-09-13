ad8db7c2ab15d19cc3527aec9c0b9147a03baf86
lubuntum <lubuntum@gmail.com>
2026-09-14 06:48:11 +0700

    Поправил пути в тестовых запросах + добавил age

--- test_data/math_predict.bat
+++ test_data/math_predict.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Sending prediction request...
-curl -X POST http://localhost:8000/predict-math -H "Content-Type: application/json" -d @prediction_request.json
+curl -X POST http://localhost:8000/predict/math -H "Content-Type: application/json" -d @prediction_request.json
 echo.
 pause
